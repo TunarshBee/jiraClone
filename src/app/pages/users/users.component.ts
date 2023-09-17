c9e536545ea7cc5635d3480e6a86bcdbd76a45d6
TunarshBee <tunarsh4real@gmail.com>
2023-09-17 07:13:27 -0700

    Done with getting all user and creating a user

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
-    
+export class UsersComponent implements OnInit {
+ userList:any[]=[]
+ userObj: any ={
+  "userId": 0,
+  "emailId": "",
+  "fullName": "",
+  "password": ""
+}
+    constructor (private http: HttpClient){
+  
+    }
+ngOnInit(): void {
+  this.getAvailableUsers()
+}
+  
+    getAvailableUsers(){
+      this.http.get('https://freeapi.miniprojectideas.com/api/Jira/GetAllUsers').subscribe((res:any)=>{
+        this.userList = res.data
+        console.log(this.userList);
+        
+      })
+    }
+    onSave(){
+      this.http.post('https://freeapi.miniprojectideas.com/api/Jira/CreateUser', this.userObj).subscribe((res:any)=>{
+       if (res.result) {
+        alert(res.message)
+        this.getAvailableUsers()
+       } else {
+        alert(res.message)
+       }
+      })
+    }
 }
